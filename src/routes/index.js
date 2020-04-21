a03d7270d9abd264ced6bba15a4b0b027b32e533
Jesus Alfredo Maldonado Cruz <boomer-ga@hotmail.com>
2020-04-21 18:42:20 -0500

    casi listo el index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router=express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('Pendente renderizado de principal');
+    res.render('index');
 });
  
 /* 
