b7502a360cda9363371ac2e4015bbf1a70253729
Jesus Alfredo Maldonado Cruz <boomer-ga@hotmail.com>
2020-04-24 23:44:01 -0500

    preparado para oredenamiento pd. agregar al stack codigo de ordenamiento

--- src/routes/interviews.js
+++ src/routes/interviews.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const pool = require("../databaseInterviews");
 
 router.get('/',async (req,res)=>{
-   const Interviews = await pool.query('SELECT idInterview,idSubject,idInterviewer FROM austenriggs.interviews');
-   //console.log(Interviews);
+   const Interviews = await pool.query('SELECT idInterview,idSubject,idInterviewer FROM austenriggs.interviews order by idInterview');
+   console.log(Interviews);
    res.render('interviews/all',{Interviews}); 
 });
 router.get('/watch/:id',async (req,res)=>{
    const {id} = req.params;
-   console.log(id);
-;  const interview = await pool.query('select content from austenriggs.interviews where idInterview = ?',[id]);
-   res.render('interviews/watch',{interview:interview[0]}); 
+   const interview = await pool.query('select content from austenriggs.interviews where idInterview = ?',[id]);
+   res.render('interviews/watch',{interview:interview[0],idInterview:id}); 
+
 });
 
 module.exports = router;
