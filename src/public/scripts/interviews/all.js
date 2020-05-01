276d6304a85796c4c3dd5848237448419cb12dbe
Jesus Alfredo Maldonado Cruz <boomer-ga@hotmail.com>
2020-05-01 07:16:54 -0500

    Annadidos nuevos elementos de etiuetado por color

--- src/public/scripts/interviews/all.js
+++ src/public/scripts/interviews/all.js
@@ -3,6 +3,9 @@
 function getTextArea(){
   return document.getElementById("text_html");
 }
+function getContTextArea(){
+  return document.getElementById("contTextArea");
+}
 function getResultArea(){
   return document.getElementById("result");
 }
@@ -12,33 +15,30 @@ function getButton(){
 function getColor(){
   return document.getElementById("selectedColor");
 }
-function processSelectedText(pElementHtml,pStarts,pEnds)
-{
-    return pElementHtml.slice(pStarts,pEnds);  
-}
 function getSelectedText()
 {
-  var textArea = getTextArea();
-  var start = textArea.selectionStart;
-  var end    = textArea.selectionEnd;
-  getResultArea().innerHTML=processSelectedText(textArea.value,start,end);
+  getResultArea().innerHTML=window.getSelection().toString();
 }
 function reProcessText()
 {
-  alert('presionado');
-  var actualText = getTextArea().value;
   var selectedText = getResultArea().value;
-  var newText = actualText.replace(selectedText,'<span style="color:' + getColor().value +'">' + selectedText + '</span>');
-  getTextArea().innerHTML = newText;
+  var allText = getTextArea().innerHTML;
+  var style ='style="background:'+getColor().value+'"';
+  var outputText = allText.replace(selectedText,'<span '+style+'>'+selectedText+'</span>');
+  getTextArea().remove();
+  var child = document.createElement("div");
+  child.id = "text_html";
+  console.log(outputText);
+  child.innerHTML = outputText.toString();
+  document.getElementById('contTextArea').appendChild(child);
+  
 }
 function addEvents(html_textArea,html_Button){
   var eventos = ["keyup","keydown","mousedown","mouseup","mousemove"];
   for(var i in eventos){
     html_textArea.addEventListener( eventos[i], getSelectedText);
   }
-  console.log(html_Button);
   html_Button.addEventListener("click",reProcessText)
 }
+addEvents(getContTextArea(),getButton());
 
-
-addEvents(getTextArea(),getButton());
--- src/routes/interviews.js
+++ src/routes/interviews.js
@@ -11,8 +11,8 @@ router.get('/',async (req,res)=>{
 router.get('/watch/:id',async (req,res)=>{
    const {id} = req.params;
    const interview = await pool.query('select content from austenriggs.interviews where idInterview = ?',[id]);
-   console.log(interview.keys());
-   res.render('interviews/watch',{interview:interview[0].content,idInterview:id}); 
+   //console.log(interview[0].content.split("\n"));
+   res.render('interviews/watch',{interview:interview[0].content.split("\n"),idInterview:id}); 
 });
 
 module.exports = router;
